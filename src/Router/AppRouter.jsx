44a1736811739dc520217383ffb38670ebc80310
Erickc404 <erick.morales.isw@gmail.com>
2024-08-19 01:24:30 -0600

    cambio rutas

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -4,29 +4,24 @@ import { PublicRoutes } from "./PublicRoutes";
 import { PageNotFound } from "./NotFound";
 import { Home, Servicios, Bodas, Fiestas, Especiales, Galeria, Contacto, Mobiliario, NuestrosEventos, locationPage } from "../lazyImplemetation/LazyGeneral";
 
-
-
 export const AppRouter = () => {
     return (
         <Routes>
             <Route element={<LayoutPublic />}>
                 <Route element={<PublicRoutes />}>
-                    <Route path="/" Component={Home} element={<Home />} />
-                    <Route path="/Servicios" Component={Servicios} element={<Servicios />} />
-                    <Route path="/Bodas" Component={Bodas} element={<Bodas />} />
-                    <Route path="/Fiestas" Component={Fiestas} element={<Fiestas />} />
-                    <Route path="/Especiales" Component={Especiales} element={<Especiales />} />
-                    <Route path="/Galeria" Component={Galeria} element={<Galeria />} />
-                    <Route path="/Contacto" Component={Contacto} element={<Contacto />} />
-                    <Route path="/Mobiliario" Component={Mobiliario} element={<Mobiliario />} />
-                    <Route path="/Portafolio" Component={NuestrosEventos} element={<NuestrosEventos />} />
-                    <Route path="/Ubicacion" Component={locationPage} element={<locationPage />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/Servicios" element={<Servicios />} />
+                    <Route path="/Bodas" element={<Bodas />} />
+                    <Route path="/Fiestas" element={<Fiestas />} />
+                    <Route path="/Especiales" element={<Especiales />} />
+                    <Route path="/Galeria" element={<Galeria />} />
+                    <Route path="/Contacto" element={<Contacto />} />
+                    <Route path="/Mobiliario" element={<Mobiliario />} />
+                    <Route path="/Portafolio" element={<NuestrosEventos />} />
+                    <Route path="/Ubicacion" element={<locationPage />} />
                 </Route>
             </Route>
-            <Route
-                path="*"
-                element={<PageNotFound />}
-            />
+            <Route path="*" element={<PageNotFound />} />
         </Routes>
     );
 };
